7fdddbd693321ce28323480be932621debdb17f3
Eric Jacobs <eric@chomp.us>
2012-05-06 20:19:41 -0600

    Fixed dependency and added a link shortener.

--- main.js
+++ main.js
@@ -12,15 +12,16 @@
         , secure: config.secure
         , selfSigned: config.selfSigned
         , floodProtectionDelay: config.floodProtectionDelay
-        , password: config.password
+   //     , password: config.password
       })
     , maxCommitMsg = 140
   /* HTTP Stuff */
     , gitReceive = http.createServer()
+    , shortServer = config.shortener.host
     ;
 
   /* IRC Stuff */  
-  ircClient.addListener('message' + channels[0], function(user, message) {
+  ircClient.addListener('message' + config.channels[0], function(user, message) {
     var messageArray
       , userToSpam
       , spamMessage
@@ -28,12 +29,43 @@
     console.log(user + ' => ' + message);
 
     if(/telenull_bot/.test(message)) {
-      ircClient.say(channels[0], "I'm a hippopotamus!");
+      ircClient.say(config.channels[0], "I'm a hippopotamus!");
     }
 
   });
 
   /* http stuff */
+
+  function getShortLink(longLink, cb) {
+    var sendData = JSON.stringify({ target: longLink })
+      , reqOps = {
+          host: shortServer
+        , port: 80
+        , path: '/shorten'
+        , method: 'POST'
+        , headers: {
+              "Content-type": 'application/json'
+            , "Content-length": sendData.length
+          }
+        }
+      , req = http.request(reqOps, function(res) {
+          res.setEncoding('utf8');
+          res.on('data', function(data) {
+            data = JSON.parse(data);
+            if(!data.hasOwnProperty('url')) {
+              console.err('Problem calling shortener...');
+              cb(longLink);
+              return;
+            }
+            cb(data.url);
+          });
+        })
+      ;
+    
+    req.write(sendData);
+    req.end();
+  }
+
   gitReceive.on('request', function(req,res) {
     var rawData = ''
       , data
@@ -48,27 +80,31 @@
     req.on('end', function() {
       data = JSON.parse(rawData);
       data.commits.forEach(function(commit, index) {
+        var link
+          ;
+
         if(commit.message.length > maxCommitMsg) {
           commit.message = commit.message.substring(0, maxCommitMsg)
           + '[...]';
         }
 
-        ircClient.say(channels[0],
-          irc.colors.wrap('magenta', data.repository.name)
-          + '|' 
-          + irc.colors.wrap('light_cyan', commit.committer.username)
-          + ': '
-          + commit.message
-          + irc.colors.wrap('magenta', ' >> ')
-          + commit.url //TODO shorten links (write a shortenener)
-        );
+        getShortLink(commit.url, function(shortLink) {
+
+          ircClient.say(config.channels[0],
+            irc.colors.wrap('magenta', data.repository.name)
+            + '|' 
+            + irc.colors.wrap('light_cyan', commit.committer.username)
+            + ': '
+            + commit.message
+            + irc.colors.wrap('magenta', ' >> ')
+            + shortLink
+          );
+
+        });
       });
     });
 
   });
   gitReceive.listen(63000);
-
-
-
 
 }());
